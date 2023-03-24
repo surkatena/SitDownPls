7c48b01bc8f1c1592ae409db08f065ebc95b8209
ekaterina gisinova <gisinovaekaterina@yandex.ru>
2023-03-24 14:53:37 +0300

    footer is ready

--- src/resources/header.js
+++ src/resources/header.js
@@ -2,57 +2,61 @@ function header() {
   console.log('header.js');
   const header = document.querySelector('.header');
   header.innerHTML = 
-  `<div class="container">
-    <div class="header__top">
-      <div class="header__contacts">
-        <div class="header__region region">
-          <label class="region__label">
-            <span class="region__text"> Ваш регион:</span>
-            <div class="region__select select" id="regionSelect" tabindex="1">
-              <div class="region__select__header select__header">
-                <span class="region__select__current select__current">Москва</p>
+  `<div class="header__top">
+    <div class="container">
+      <div class="header__wrapper">
+        <div class="header__contacts">
+          <div class="header__region region">
+            <label class="region__label">
+              <span class="region__text"> Ваш регион:</span>
+              <div class="region__select select" id="regionSelect" tabindex="1">
+                <div class="region__select__header select__header">
+                  <span class="region__select__current select__current">Москва</p>
+                </div>
+                <div class="region__dropdown dropdown">
+                  <span class="region__dropdown__item dropdown__item visually-hidden">Москва</span>
+                  <span class="region__dropdown__item dropdown__item">Казань</span>
+                  <span class="region__dropdown__item dropdown__item">Уфа</span>
+                  <span class="region__dropdown__item dropdown__item">Пермь</span>
+                </div>
               </div>
-              <div class="region__dropdown dropdown">
-                <span class="region__dropdown__item dropdown__item visually-hidden">Москва</span>
-                <span class="region__dropdown__item dropdown__item">Казань</span>
-                <span class="region__dropdown__item dropdown__item">Уфа</span>
-                <span class="region__dropdown__item dropdown__item">Пермь</span>
-              </div>
-            </div>
-          </label>
+            </label>
+          </div>
+          <div class="header__phone">
+            <svg class="header__phone__icon" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M16.3425 12.0983C15.215 12.0983 14.1242 11.915 13.1067 11.585C12.7858 11.475 12.4283 11.5575 12.1808 11.805L10.7417 13.6108C8.1475 12.3733 5.71833 10.0358 4.42583 7.35L6.21333 5.82833C6.46083 5.57167 6.53417 5.21417 6.43333 4.89333C6.09417 3.87583 5.92 2.785 5.92 1.6575C5.92 1.1625 5.5075 0.75 5.0125 0.75H1.84083C1.34583 0.75 0.75 0.97 0.75 1.6575C0.75 10.1733 7.83583 17.25 16.3425 17.25C16.9933 17.25 17.25 16.6725 17.25 16.1683V13.0058C17.25 12.5108 16.8375 12.0983 16.3425 12.0983Z" fill="#FF862F"/>
+            </svg>
+            <a href="tel:+74958854547" class="header__phone__number" >+7 (495) 885-45-47</a>
+          </div>
         </div>
-        <div class="header__phone">
-          <svg class="header__phone__icon" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16.3425 12.0983C15.215 12.0983 14.1242 11.915 13.1067 11.585C12.7858 11.475 12.4283 11.5575 12.1808 11.805L10.7417 13.6108C8.1475 12.3733 5.71833 10.0358 4.42583 7.35L6.21333 5.82833C6.46083 5.57167 6.53417 5.21417 6.43333 4.89333C6.09417 3.87583 5.92 2.785 5.92 1.6575C5.92 1.1625 5.5075 0.75 5.0125 0.75H1.84083C1.34583 0.75 0.75 0.97 0.75 1.6575C0.75 10.1733 7.83583 17.25 16.3425 17.25C16.9933 17.25 17.25 16.6725 17.25 16.1683V13.0058C17.25 12.5108 16.8375 12.0983 16.3425 12.0983Z" fill="#FF862F"/>
-          </svg>
-          <a href="tel:+74958854547" class="header__phone__number" >+7 (495) 885-45-47</a>
+        <div class="header__info">
+          <ul class="header__info__list">
+            <li class="header__info__item">
+              <a href="#" class="header__info__link">
+                О&nbsp;компании
+              </a>
+            </li>
+            <li class="header__info__item">
+              <a href="#" class="header__info__link">
+                Гарантия и&nbsp;возврат
+              </a>
+            </li>
+            <li class="header__info__item">
+              <a href="#" class="header__info__link">
+                Корпоративным клиентам
+              </a>
+            </li>
+            <li class="header__info__item">
+              <a href="#" class="header__info__link">
+                Дизайн-решение
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div class="header__info">
-        <ul class="header__info__list">
-          <li class="header__info__item">
-            <a href="#" class="header__info__link">
-              О&nbsp;компании
-            </a>
-          </li>
-          <li class="header__info__item">
-            <a href="#" class="header__info__link">
-              Гарантия и&nbsp;возврат
-            </a>
-          </li>
-          <li class="header__info__item">
-            <a href="#" class="header__info__link">
-              Корпоративным клиентам
-            </a>
-          </li>
-          <li class="header__info__item">
-            <a href="#" class="header__info__link">
-              Дизайн-решение
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
+  </div>
+  <div class="container">
     <div class="header__middle">
       <div class="header__logo">
         <svg>
