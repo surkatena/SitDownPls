592dc1e18d288d2cc3b4d8b4ca40d25a822111cf
ekaterina gisinova <gisinovaekaterina@yandex.ru>
2023-03-30 15:27:19 +0300

    partnership.html - media is ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 console.log('main.js');
+
+// Работа кнопки бургер меню
+
+const burgerBtn = document.querySelector('.burger');
+const navItems = [...document.querySelectorAll('.nav__link')];
+
+burgerBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  burgerBtn.classList.toggle('burger-open');
+})
+
+for (let item of navItems) {
+  item.addEventListener('click', function() {
+    if (this.closest('nav')) {
+      burgerClose()
+    }
+  })
+}
+
+
+
+function burgerClose() {
+  burgerBtn.classList.remove('burger-open');
+}
